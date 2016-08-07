a556dc9eaef8a731bc6a00751594994f54c68238
Joshua Mummert <jm@joshuamummert.com>
2016-08-07 13:06:43 -0400

    added some random local scraps

--- grep_sed_stuff.sh
+++
@@ -1,2 +0,0 @@
-# Recursively search and place string in any directory despite structure
-	grep -lR --exclude-dir=dirname 'matchPattern' path/to/dir/ | xargs sed -i "s/matchPattern/replacePattern/g"
