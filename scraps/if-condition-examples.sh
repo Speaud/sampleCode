66dc62dd2f6c58795b89bcf6bd4ba8e91ef9dec7
Joshua Mummert <jm@joshuamummert.com>
2018-12-11 15:17:16 -0500

    working on version logic stuff for work, added some refs and exs for conditional expressions

--- scraps/if-condition-examples.sh
+++ scraps/if-condition-examples.sh
@@ -1,6 +1,27 @@
+# http://tldp.org/LDP/Bash-Beginners-Guide/html/sect_07_01.html
+# https://www.gnu.org/software/bash/manual/bash.html#Bash-Conditional-Expressions
+# http://tldp.org/HOWTO/Bash-Prog-Intro-HOWTO-11.html
+
 # if directory exists
 if [ ! -d "$BUILD_DIRECTORY" ]; then
   # Control will enter here if $DIRECTORY doesn't exist.
   echo 'app dir not found'
   exit 1
+fi
+
+# (a)
+if [ $USE_DEFAULT_VERSIONING_LOGIC != "y" ]; then
+    echo "invalid"
+fi
+
+# shorthand for (a)
+[ $USE_DEFAULT_VERSIONING_LOGIC != "y" ] && ( echo you are using a non-privileged account; exit 1 )
+
+
+if [ "$seconds" -eq 0 ]; then
+   timezone_string="Z"
+elif [ "$seconds" -gt 0 ]; then
+   timezone_string=$(printf "%02d:%02d" $((seconds/3600)) $(((seconds / 60) % 60)))
+else
+   echo "Unknown parameter"
 fi
--- scraps/sandbox.sh
+++ scraps/sandbox.sh
@@ -1,21 +1,24 @@
-#!/bin/bash4
+#!/usr/bin/env bash
 
-declare -A address
-#       -A option declares associative array.
+DetermineVersioningLogic() {
+    echo "Use default versioning logic? (y/n)"
+    read use_default_versioning_logic
 
-address[Charles]="414 W. 10th Ave., Baltimore, MD 21236"
-address[John]="202 E. 3rd St., New York, NY 10009"
-address[Wilma]="1854 Vermont Ave, Los Angeles, CA 90023"
+    [[ ! $use_default_versioning_logic =~ ^[yn]$ ]] && ( echo "Invalid entry"; DetermineVersioningLogic; )
+    [[ $use_default_versioning_logic == "y" ]] && ( HandleUserDefinedVersion; )
+}
 
+HandleUserDefinedVersion() {
+    echo "Enter the explicit version: "
+    read explicit_version
 
-echo "Charles's address is ${address[Charles]}."
-# Charles's address is 414 W. 10th Ave., Baltimore, MD 21236.
-echo "Wilma's address is ${address[Wilma]}."
-# Wilma's address is 1854 Vermont Ave, Los Angeles, CA 90023.
-echo "John's address is ${address[John]}."
-# John's address is 202 E. 3rd St., New York, NY 10009.
+    if [[ ! $explicit_version =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+        echo "Invalid entry. The version must match the following pattern"
+        HandleUserDefinedVersion
+    else
+        echo "all good"
+    fi
+    # git tag -l <pattern>
+}
 
-echo
-
-echo "${!address[*]}"   # The array indices ...
-# Charles John Wilma
+DetermineVersioningLogic
