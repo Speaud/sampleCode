9dceb7ed352e8bc004aef847fd6ba470725e0994
Joshua Mummert <jm@joshuamummert.com>
2019-05-04 19:13:58 -0400

    cls local dev

--- scripting/python/sandbox/main.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-from hello import helloworld
-print("This line will be printed.")
-helloworld()
--- scripting/python3/sandbox/threadinit.py
+++ scripting/python3/sandbox/threadinit.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 # Python program to illustrate the concept 
 # of threading 
+# concurrent access of threads to the shared variable cause race condition
+# however if the shared variable is static you should be good...?
 import os 
 import time
 import threading 
   
-def task1(): 
-    print("Task 1 assigned to thread: {}".format(threading.current_thread().name))
-    time.sleep( 5 )
+def task1(_p): 
+    #time.sleep( 5 )
+    print('_p=', _p)
+    print("Task{} 1 assigned to thread: {}".format(_p, threading.current_thread().name))
     print("ID of process running task 1: {}".format(os.getpid())) 
   
 def task2(): 
@@ -20,10 +23,10 @@ if __name__ == "__main__":
     print("ID of process running main program: {}".format(os.getpid())) 
   
     # print name of main thread 
-    print("Main thread name: {}".format(threading.main_thread().name)) 
+    print("Main thread name: {}".format(threading.main_thread().name))
   
     # creating threads 
-    t1 = threading.Thread(target=task1, name='t1') 
+    t1 = threading.Thread(target=task1, name='t1', args=("asd",)) 
     t2 = threading.Thread(target=task2, name='t2')   
   
     # starting threads 
