b1e5aacb2f90275fdf6e72f280356284eb19123d
Joshua Mummert <jm@joshuamummert.com>
2021-12-25 08:25:52 -0500

    Add go example using command line flags

---
+++ go/command_line_flags.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+/*
+	command_line_flags -word=opt -numb=7 -fork -svar=flag
+	-word=opt
+	-word=opt a1 a2 a3
+	-word=opt a1 a2 a3 -numb=7
+	-h
+	-wat
+*/
+
+func main() {
+
+	wordPtr := flag.String("word", "foo", "a string")
+
+	numbPtr := flag.Int("numb", 42, "an int")
+	forkPtr := flag.Bool("fork", false, "a bool")
+
+	var svar string
+	flag.StringVar(&svar, "svar", "bar", "a string var")
+
+	flag.Parse()
+
+	fmt.Println("word:", *wordPtr)
+	fmt.Println("numb:", *numbPtr)
+	fmt.Println("fork:", *forkPtr)
+	fmt.Println("svar:", svar)
+	fmt.Println("tail:", flag.Args())
+}
