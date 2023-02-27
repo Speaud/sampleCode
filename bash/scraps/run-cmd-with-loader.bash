a43ec2878c8e8be56ef03ede266b4503b8e11d5a
speaud <jm@joshuamummert.com>
2023-02-26 23:27:40 -0500

    Clean up formatting

--- bash/scraps/run-cmd-with-loader.bash
+++ bash/scraps/run-cmd-with-loader.bash
@@ -41,31 +41,32 @@ ExeCmd(){
 		[[ $(egrep -c '[a-zA-Z0-9]*' $ERRLOG) -ne 0 ]] && { printf "\b\e[91m\u00D7$ODEFAULT]\n"; PromptUsr 4 "See $ERRLOG for more information."; } || printf "\b\e[32m\u2713$ODEFAULT]\n"
 	}
 }
-	floor() {
-	  DIVIDEND=${1}
-	  DIVISOR=${2}
-	  RESULT=$(( ( ${DIVIDEND} - ( ${DIVIDEND} % ${DIVISOR}) )/${DIVISOR} ))
-	  echo ${RESULT}
-	}
 
-	timecount(){
-	  s=${1}
-	  HOUR=$( floor ${s} 60/60 )
-	  s=$((${s}-(60*60*${HOUR})))
-	  MIN=$( floor ${s} 60 )
-	  SEC=$((${s}-60*${MIN}))
-	  while [ $HOUR -ge 0 ]; do
-      while [ $MIN -ge 0 ]; do
-          while [ $SEC -ge 0 ]; do
-            printf "$sii Deploying to Test in %02d:%02d:%02d\033[0K\r" $HOUR $MIN $SEC
-            SEC=$((SEC-1))
-            sleep 1
-          done
-        SEC=59
-        MIN=$((MIN-1))
-      done
-      MIN=59
-      HOUR=$((HOUR-1))
-	  done
-	  printf "\n"
-	}  	
+floor() {
+	DIVIDEND=${1}
+	DIVISOR=${2}
+	RESULT=$(( ( ${DIVIDEND} - ( ${DIVIDEND} % ${DIVISOR}) )/${DIVISOR} ))
+	echo ${RESULT}
+}
+
+timecount(){
+	s=${1}
+	HOUR=$( floor ${s} 60/60 )
+	s=$((${s}-(60*60*${HOUR})))
+	MIN=$( floor ${s} 60 )
+	SEC=$((${s}-60*${MIN}))
+	while [ $HOUR -ge 0 ]; do
+	while [ $MIN -ge 0 ]; do
+		while [ $SEC -ge 0 ]; do
+		printf "$sii Deploying to Test in %02d:%02d:%02d\033[0K\r" $HOUR $MIN $SEC
+		SEC=$((SEC-1))
+		sleep 1
+		done
+	SEC=59
+	MIN=$((MIN-1))
+	done
+	MIN=59
+	HOUR=$((HOUR-1))
+	done
+	printf "\n"
+}  	
