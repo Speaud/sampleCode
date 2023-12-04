440cf0da1c1dd2f57f26588aa57106565ca0b3de
speaud <jm@joshuamummert.com>
2023-12-04 06:43:23 -0500

    Just playing

--- bash/scraps/condition-examples.bash
+++ bash/scraps/condition-examples.bash
@@ -3,26 +3,26 @@
 # http://tldp.org/HOWTO/Bash-Prog-Intro-HOWTO-11.html
 
 # if directory exists
-if [ ! -d "$BUILD_DIRECTORY" ]; then
-  echo 'app dir not found'
+if [ ! -d "foo" ]; then
+  echo "dir not found"
   exit 1
 fi
 
 # (a)
-if [ $USE_DEFAULT_VERSIONING_LOGIC != "y" ]; then
+if [ "y" != "y" ]; then
     echo "invalid"
 fi
 
 # shorthand for (a)
-[ $USE_DEFAULT_VERSIONING_LOGIC != "y" ] && ( echo you are using a non-privileged account; exit 1 )
+[ "y" != "y" ] && ( echo you are using a non-privileged account; exit 1 )
 
 # nested if else
 [ $(date +%w) -eq 6 ] && {echo "do something on Saturdays"; do_some_other_stuff; exit 0;} || echo "do different things on other days"
 
-if [ "$seconds" -eq 0 ]; then
+if [ "0" -eq 0 ]; then
    timezone_string="Z"
-elif [ "$seconds" -gt 0 ]; then
-   timezone_string=$(printf "%02d:%02d" $((seconds/3600)) $(((seconds / 60) % 60)))
+elif [ "0" -gt 0 ]; then
+   timezone_string=$(printf "%02d:%02d" $((0/3600)) $(((0/ 60) % 60)))
 else
    echo "Unknown parameter"
 fi
